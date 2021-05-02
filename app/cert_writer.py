a5d69235bb896f3420a0322d994a3112ead59d2c
Philip J Spelman <phil_spelman@hotmail.com>
2021-05-01 22:02:26 -0400

    - cert_writer.py   - updated methods to use a different approach for editing docx file   - set up the template docx file in private directory at project root - mailer.py   - implemented attaching a file to the message   - switched to using SSL connection for now (currently only using smpt.gmail.com)   - NOTE: It was necessary to enable less secure app access on gmail to be able to log in - added docx2pdf to the repo for the time being - looking for a different solution

--- app/cert_writer.py
+++ app/cert_writer.py
@@ -2,18 +2,20 @@ import docx
 import csv
 import time
 import zipfile
+from docx2pdf import convert
 
 
 # Cert writer
-def create_certificate(attendee_name: str, meet_date: str):
+def create_certificate(attendee_name: str, training_date: str):
     print(f"trying to create cert for {attendee_name}")
-    replace_text_fields = {"«FIRST» «LAST»": attendee_name,
-                           "XXXMEETDATEXXX": meet_date}
-    base_docx_file = "certificate_of_completion_cpy.docx"
-    new_docx_file = f"hart3s_harm_reduction_certificate_{attendee_name.replace(' ', '_')}.docx"
+    replace_text_fields = {"XXXCLIENTNAMEXXX": attendee_name,
+                           "XXXMEETDATEXXX": training_date}
+    base_docx_file = "../private/certificate_of_completion_template.docx"
+    docx_output_dir = "./docx_temp"
+    new_docx_file = f"{docx_output_dir}/hart3s_harm_reduction_certificate_{attendee_name.replace(' ', '_')}.docx"
     docx_template = zipfile.ZipFile(base_docx_file)
 
-    new_docx = zipfile.ZipFile(new_docx_file, "x")  # X for exclusive create mode | a for append
+    new_docx = zipfile.ZipFile(new_docx_file, "w")  # X for exclusive create mode | a for append | w for write
     with open(docx_template.extract("word/document.xml", "./")) as tempXmlFile:
         xml_template_string = tempXmlFile.read()
         for key in replace_text_fields.keys():
@@ -32,6 +34,18 @@ def create_certificate(attendee_name: str, meet_date: str):
     new_docx.close()
 
 
+def convert_to_pdf(path_to_convert: str, del_docx_after: bool = False):
+    """ specify a local docx file to convert to PDF
+    :param del_docx_after: boolean | delete the original docx file after conversion
+    :param path_to_convert: str | name of the local docx file OR directory with files to be converted
+    :return:
+    """
+    pdf_output_dir = "./pdf_certs/"
+    # convert the certs_docx directory
+    print(f"trying to convert {path_to_convert} to pdf")
+    convert(path_to_convert, output_path=pdf_output_dir)
+
+
 class AttendeeTracker:
     def __init__(self, training_date: str):
         self.training_date = training_date  # training date will be printed on the certificates (e.g., APRIL 2021)
@@ -39,9 +53,7 @@ class AttendeeTracker:
         self.emails = set()
 
     def load_attendees(self):
-        # get the csv
-        #
-        certs_to_send_file = "../private/attendees_certs_todo.csv"
+        certs_to_send_file = "../private/attendees_certs_todo.csv"  # attendees file each line after line 0 should be [NAME, EMAIL]
         new_emails = set()
         new_list = list()
         with open(certs_to_send_file, newline='') as csvfile:
@@ -49,7 +61,6 @@ class AttendeeTracker:
             for n, row in enumerate(line_reader, 0):
                 if n and row:  # skip blank rows
                     try:
-                        print(row)
                         attendee_name = row[0]
                         attendee_email = row[1]
                         if attendee_email in self.emails:  # Do not allow duplicate email addresses, each attendee should have a unique email
@@ -57,7 +68,7 @@ class AttendeeTracker:
                         new_emails.add(attendee_email)
                         new_list.append(row)
                     except Exception as e:
-                        print(f"Exception encountered when trying to process row: ", row)
+                        print(f"Exception encountered when trying to process row: ", row, e)
                         if "y" not in input("Continue? (type Y or YES to continue, anything else to abort)").lower():
                             print(f"... Aborting loading")
                             return None
@@ -68,10 +79,15 @@ class AttendeeTracker:
         return self._attendee_list
 
     def make_certs(self, n_attendees=3):
-        print(f"going to make certs for up to {n_attendees} attendees")
+        if not n_attendees:
+            print(f"making certs for ALL the attendees")
+            n_attendees = len(self.attendee_list)
+        else:
+            print(f"going to make certs for up to {n_attendees} attendees")
+
         for attendee in self.attendee_list[:n_attendees]:
             print(f"making cert for {attendee}")
-
+            create_certificate(attendee[0], self.training_date)
 
     @property
     def attendee_list(self):
@@ -84,15 +100,11 @@ class AttendeeTracker:
         return f"AttendeeTracker({self.training_date})"
 
 
-# TODO: Load the base certificate of completion
-# TODO: Create new docx files with each name
-# TODO: Convert docx to PDF
-
 # training_date = time.strftime("%m/%d/%y", time.localtime(time.time()))
-training_date = "APRIL 2021"
-print(training_date)
+event_date = "APRIL 2021"
+print(event_date)
 
-tracker = AttendeeTracker(training_date)
+tracker = AttendeeTracker(event_date)
 tracker.make_certs()
 
-
+# TODO: Convert docx to PDF
--- app/mailer.py
+++ app/mailer.py
@@ -3,6 +3,8 @@ from dotenv import dotenv_values
 import json
 import getpass
 from gmail.gmail import gmail
+from email.message import EmailMessage
+import mimetypes
 
 config = dotenv_values("../.env")  # config = {"USER": "foo", "EMAIL": "foo@example.org"}
 
@@ -14,6 +16,26 @@ def login_pwd():
     if not (login_pass := config.get('EMAIL_PASS', None)):
         login_pass = getpass.getpass("Input the login password: ", )
     return login_pass
+
+
+def attach_file(message: EmailMessage, file_path: str):
+    print(f"trying to attach {file_path}")
+    # attachment
+    mime_type, _ = mimetypes.guess_type(file_path)
+    print("trying to add a file of type: ", mime_type)
+    mime_type, mime_subtype = mime_type.split('/')
+    file_name = file_path.split("/")[-1]
+    print(f"file_name: ", file_name)
+
+    with open(file_path, 'rb') as file:
+        message.add_attachment(
+            file.read(),
+            maintype=mime_type,
+            subtype=mime_subtype,
+            filename=file_name
+        )
+
+    return message
 
 
 class Mailer:
@@ -31,24 +53,40 @@ class Mailer:
     def start_server(self):
         if not self.__server:
             print(f"starting server for the first time | connecting to {self.__smtp_backend} : {self.__smtp_port}")
-        s = smtplib.SMTP(self.__smtp_backend, self.__smtp_port)
+        # s = smtplib.SMTP(self.__smtp_backend, self.__smtp_port)
+        # s = smtplib.SMTP(self.__smtp_backend, self.__smtp_port)
+        s = smtplib.SMTP_SSL(self.__smtp_backend)
+        s.set_debuglevel(1)  # add some debugging
         print(f"calling s.ehlo()")
         s.ehlo()
         print(f"calling s.starttls()")
-        s.starttls()
+        # s.starttls()
         password = login_pwd()
         print(f"trying to login with {self.login_user} | p: {password}")
         s.login(self.login_user, password)
         self.__server = s
         return self.__server
 
-    def send_mail(self, message: str, email_to: str, attachments: list) -> None:
-        print(f"trying to send email from {self.login_user} to {email_to} with {len(attachments)} attachments")
-        self.server.sendmail(
-            from_addr=self.login_user,
-            to_addrs=email_to,
-            msg=message
-        )
+    def send_mail(self, email_body: str, subj: str,
+                  email_to: str, attachment_file: str = None) -> None:
+        message = EmailMessage()
+        message['From'] = self.login_user
+        message['To'] = email_to
+        message['Subject'] = subj
+        message.set_content(email_body)
+        if attachment_file:
+            attach_file(message, attachment_file)
+
+        print(f"trying to send email from {self.login_user} to {email_to} {'with attachment' if attachment_file else ''}")
+        self.server.send_message(message)
+        print(f"QUITTING THE MAIL SERVER")
+        self.server.quit()
+        self.__server = None
+        # self.server.sendmail(
+        #     from_addr=self.login_user,
+        #     to_addrs=[email_to],
+        #     msg=message
+        # )
 
     def test(self):
         mail = gmail()
@@ -71,3 +109,16 @@ class Mailer:
 if __name__ == '__main__':
     print(f"mailer = Mailer()")
     mailer = Mailer()
+    try:
+        mailer.start_server()
+        sample_attachment = "./pdf_certs/hart3s_harm_reduction_certificate_Allison_Edwards.pdf"
+        mail_body = "Attached you will find a certificate of completion for the HaRT3S training"
+        email_subject = "Testing sending out certificates"
+        recipient = "phil.spelman@gmail.com"
+        if "y" in input("Continue? (type Y or YES to continue, anything else to abort)").lower():
+            mailer.send_mail(mail_body, email_subject, recipient, sample_attachment)
+    except Exception as e:
+        print(f"Exception encountered when trying to send stuff: ", e)
+    finally:
+        print(f"closing connection")
+        mailer.server.quit()
